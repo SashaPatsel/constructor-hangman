db2eb8c377e71f45d1e721fcf58d938ea98d4e48
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-02-18 14:50:28 -0800

    letter.js finished (for now)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var Word = require("./word.js");
-var inquirer = require("inquirer");
+// var Word = require("./word.js");
+// var inquirer = require("inquirer");
 
 
 // index.js: The file containing the logic for the course of the game, which depends on Word.js and:
 // Randomly selects a word and uses the Word constructor to store it
 // Prompts the user for each guess and keeps track of the user's remaining guesses
+
--- letter.js
+++ letter.js
@@ -1,22 +1,29 @@
-function Letter (){
-  this.letter = 
-  this.guess = false
-  this.goodGuess = function() {
+ function Letter(ltr) {
+     this.ltr = ltr
+     this.guess = false
+     this.guessCheck = function() {
+         if (this.guess) {
+            return this.ltr
+         } else {
+          return " _ "
+         }
+     }
+     this.goodGuess = function(char) {
+         if (char === this.ltr) {
+             guess = true;
+         }
+     }
 
-  }
-  this.guessCheck = function(char) {
-
-  }
-
-}
+ }
 
 
 
-module.exports = {
-  Letter: Letter,
-}
+ module.exports = {
+     Letter: Letter,
+ }
 
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+ //Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+ // A string value to store the underlying character for the letter
+ // A boolean value that stores whether that letter has been guessed yet
+ // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+ // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
